c2addc897e945dbc3358e46d1d0892906cb7da37
mcouliba <mcouliba985@gmail.com>
2025-05-25 02:01:35 +0000

    debut de creation de l'interface UI/UX

--- front-office/astro.config.mjs
+++ front-office/astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), sitemap()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  site: "https://magservices-mali.org/",
+      integrations: [react(), sitemap()],
+      vite: {
+            plugins: [tailwindcss()],
+      },
+      site: 'https://magservices-mali.org/',
 });
